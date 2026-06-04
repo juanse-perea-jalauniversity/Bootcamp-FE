fe55f773dba12c2934851d5837588c00d3bf24cd
juanse-perea-jalauniversity <juan.perea@jala.university>
2026-06-04 15:49:37 -0500

    feat: add for experiment (for now commented)

--- signals-layout/src/app/layout/layout.ts
+++ signals-layout/src/app/layout/layout.ts
@@ -11,4 +11,12 @@ export class Layout {
 
   protected collapse = () => this.isCollapsed.update(state => !state)
 
+  // For experiment
+  // items = [12, 43, 1213, 534234]
+  // itemsSignal = signal([12, 43, 1213, 534234])
+
+  // log = (logInput: number) => {
+  //   console.log(logInput)
+  //   return
+  // }
 }
