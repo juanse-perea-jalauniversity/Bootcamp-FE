572659d8c01322b1c0db278d714f213432cb287e
juanse-perea-jalauniversity <juan.perea@jala.university>
2026-06-23 13:19:55 -0500

    feat: add options for photo size. Also add missing semi colons.

--- web-component/jala-card.js
+++ web-component/jala-card.js
@@ -2,17 +2,17 @@
 class JalaCard extends HTMLElement {
   constructor() {
     super();
-    this._root = this.attachShadow({ mode: 'closed' })
+    this._root = this.attachShadow({ mode: 'closed' });
   }
 
   connectedCallback() {
-    const imgSrc = this.getAttribute('img-src') ?? './me-anime.png'
-    const name = this.getAttribute('name') ?? 'Perea Juan sama'
-    const role = this.getAttribute('role') ?? 'Senior Architect'
-    const titleColor = this.getAttribute('title-color') ?? '#fff'
-    const contentColor = this.getAttribute('content-color') ?? '#282828'
-    const footerColor = this.getAttribute('footer-color') ?? '#ee283f'
-    const cardTemplate = document.querySelector("#jala-card-template").content.cloneNode(true)
+    const imgSrc = this.getAttribute('img-src') ?? './me-anime.png';
+    const name = this.getAttribute('name') ?? 'Perea Juan sama';
+    const role = this.getAttribute('role') ?? 'Senior Architect';
+    const titleColor = this.getAttribute('title-color') ?? '#fff';
+    const contentColor = this.getAttribute('content-color') ?? '#282828';
+    const footerColor = this.getAttribute('footer-color') ?? '#ee283f';
+    const cardTemplate = document.querySelector("#jala-card-template").content.cloneNode(true);
     cardTemplate.querySelector(".id-photo").src = imgSrc;
     cardTemplate.querySelector(".name").textContent = name;
     cardTemplate.querySelector(".role").textContent = role;
@@ -20,7 +20,7 @@ class JalaCard extends HTMLElement {
     cardTemplate.querySelector(".content").style.backgroundColor = contentColor;
     cardTemplate.querySelector(".footer").style.backgroundColor = footerColor;
 
-    this._root.appendChild(cardTemplate)
+    this._root.appendChild(cardTemplate);
 
   }
 }
