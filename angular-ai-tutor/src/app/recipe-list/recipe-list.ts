2a2bd7ff3583055f737ccdd7ff5e4b970f497abc
juanse-perea-jalauniversity <juan.perea@jala.university>
2026-06-29 14:51:47 -0500

    feat: module 13 two way binding

--- angular-ai-tutor/src/app/recipe-list/recipe-list.ts
+++ angular-ai-tutor/src/app/recipe-list/recipe-list.ts
@@ -2,10 +2,11 @@ import { Component, computed, signal } from '@angular/core';
 import { RecipeModel } from '../models';
 import { MOCK_RECIPES } from '../mock-recipes';
 import { RecipeDetail } from "../recipe-detail/recipe-detail";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-list',
-  imports: [RecipeDetail],
+  imports: [RecipeDetail, FormsModule],
   templateUrl: './recipe-list.html',
   styleUrl: './recipe-list.css',
 })
@@ -13,13 +14,15 @@ export class RecipeList {
   protected readonly title = signal('My Recipe Box Signal');
   protected readonly recipe = signal<RecipeModel>(MOCK_RECIPES[0]);
   protected readonly recipes = MOCK_RECIPES;
+  protected readonly searchTerm = signal("")
+  protected readonly filteredRecipes = computed(() => this.recipes.filter(rec => rec.name.toLowerCase().includes(this.searchTerm().toLowerCase())))
 
-  protected switchToCarbonara(): void {
-    this.recipe.set(MOCK_RECIPES[0]);
-  }
+  // protected switchToCarbonara(): void {
+  //   this.recipe.set(MOCK_RECIPES[0]);
+  // }
 
-  protected switchToSalad(): void {
-    this.recipe.set(MOCK_RECIPES[1]);
-  }
+  // protected switchToSalad(): void {
+  //   this.recipe.set(MOCK_RECIPES[1]);
+  // }
 
 }
