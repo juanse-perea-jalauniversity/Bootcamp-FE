b957639dc3b22164ae05117c05b2243ecd324b40
juanse-perea-jalauniversity <juan.perea@jala.university>
2026-08-19 18:44:02 -0500

    feat: finish User Story 1

--- duelist-codex-react/src/components/Home.tsx
+++ duelist-codex-react/src/components/Home.tsx
@@ -1,19 +1,32 @@
 import { useState } from "react"
 import { Header } from "./Header"
 import { SearchBar } from "./SearchBar"
-import { CardGrid } from "./CardGrid"
-import { sampleCards } from "../mock/sampleCards"
+import { CardGrid, CardGridSkeleton } from "./CardGrid"
 import "./Home.css"
+import { useFetchCards } from "../hooks/useFetchCards"
 
 export function Home() {
   const [term, setTerm] = useState("")
+  const { cards, isLoading } = useFetchCards()
 
   return (
     <div className="dc-page">
       <Header />
       <main className="dc-page__main">
         <SearchBar value={term} onChange={setTerm} onClear={() => setTerm("")} />
-        <CardGrid cards={sampleCards} onSelectCard={() => {}} />
+        {isLoading
+          ?
+          <CardGridSkeleton />
+          :
+          <></>
+        }
+        {
+          cards.length === 0
+            ?
+            <p>No cards to show</p>
+            :
+            <CardGrid cards={cards} onSelectCard={() => { }} />
+        }
       </main>
     </div>
   )
--- duelist-codex-react/src/hooks/useFetchCards.ts
+++ duelist-codex-react/src/hooks/useFetchCards.ts
@@ -4,10 +4,18 @@ import { fetchCards } from "../api/fetchCards";
 
 export function useFetchCards() {
 	const [cards, setCards] = useState<Card[]>([])
+	const [isLoading, setIsLoading] = useState<boolean>(true)
+
 	useEffect(() => {
+		setIsLoading(true)
 		fetchCards().then(res => {
 			setCards(res.data.data)
+			setIsLoading(false)
 		})
+			.catch(err => console.log("Something happened fetching the cards ", err))
 	}, [])
-	return cards
+	return {
+		cards,
+		isLoading
+	}
 }
